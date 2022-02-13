032b09cf5f4e92fcac601319e199cfd5dac54aad
Carlos Cervantes <85979189+AndreCerva@users.noreply.github.com>
2022-02-13 16:23:56 -0600

    rasp v1

---
+++ Raspberry/listening.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO#librerias que nos permite trabajar con los pines de la rasp
+import time #Libreria para tiempos de espera
+from azure.iot.device import IoTHubDeviceClient#Libreria para hacer uso del servicio de Azure IoT Hub
+GPIO.setwarnings(False)#No mostrar advertencias
+#La siguiente connection_string se encuentra en el portal de azure en el registo del device
+CONNECTION_STRING = "Aquí la cadena de conexion" #Aquí pegamos la cadena de conexion tomada desde nuestro portal de azure
+pin=16 #Pin positivo al que ira conectado el led verde
+GPIO.setmode(GPIO.BOARD) #Numeraremos los pines del rasp con la numeración board (locación fisica)
+GPIO.setup(pin,GPIO.OUT)#Indicamos que en el pin del led v estará mandando señal
+#Funcion que imprime los mensajes recibidos en la consola
+def message_handler(message): #Recibe el mensaje con todos sus atributos y metodos
+  if message.data.decode() == 'ON': #Si el mensaje que se envia es que se encontro a tommy
+      GPIO.output(pin,GPIO.LOW) #Dar una salida en bajo
+      print("Luz encendida")
+  else: #No se ha encontrado a tommy aún
+      print("Luz apagada")
+      GPIO.output(pin,GPIO.HIGH) #encender led rojo
+  input('Presiona enter para seguir leyendo mensajes')#Para seguir escuchando presionar enter
+  print ("Esperando por un mensaje...")#Estamos esperando algún mensaje
+#Funcion para inicializar el cliente y esperar a recibir el mensaje de la nube al dispositivo
+def main():
+    GPIO.output(pin,GPIO.HIGH)# iniciamos en alto el led rojo que indica que no se ha encontrado aún
+    print ("Esperando por una instrucción...")#indicamos que se empieza la busqueda
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)#instanciamos el cliente 
+    try:
+        # Attach the handler to the client
+        client.on_message_received = message_handler#Leemos el mensaje que se ha enviado a nuestro device 
+        while True: 
+            time.sleep(1000) #mil segundos esperando un mensaje, por poner un numero, puede ser otro.
+    except KeyboardInterrupt:
+        print("IoT Hub C2D Messaging stopped") #se detiene la ejecución debido a una interrupción de teclado
+    finally:
+        GPIO.cleanup()
+        print("Shutting down IoT Hub Client")#Terminamos con éxito la ejecución del programa
+        client.shutdown()#Se deja de esperar el envio de mensajes
+if __name__ == '__main__':
+    main()
